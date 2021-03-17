ebe0f1e6d8fcf016d97e0dd2991ce953457f02c6
_Lucy <80708819+Lucyucy@users.noreply.github.com>
2021-03-17 18:56:48 +0100

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,10 @@
+local modpath = minetest.get_modpath("chocolate")
+
+chocolate = {}
+
+dofile(modpath.."/nodes.lua")
+dofile(modpath.."/items.lua")
+dofile(modpath.."/crafting.lua")
+dofile(modpath.."/tools.lua")
+dofile(modpath.."/mapgen.lua")
+dofile(modpath.."/armor.lua")
