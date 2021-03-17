e06dde56aa355a362adea0e68e7dea55270bcc16
_Lucy <80708819+Lucyucy@users.noreply.github.com>
2021-03-17 18:46:22 +0100

    Create nodes.lua

---
+++ nodes.lua
@@ -0,0 +1,12 @@
+minetest.register_node("chocolate:chocolate_ore", {
+		description = "Chocolate Ore",
+		tiles = {"chocolate_ore.png"},
+		groups = {cracky = 1, level = 2},
+		drop = "chocolate:chocolate"
+})
+
+minetest.register_node("chocolate:chocolateblock", {
+		description = "Chocolate Block",
+		tiles = {"chocolate_block.png"},
+		groups = {cracky = 1, level = 3}
+})
