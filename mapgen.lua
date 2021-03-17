6690a2c4681fdefa5587382b678984b54b42a40f
_Lucy <80708819+Lucyucy@users.noreply.github.com>
2021-03-17 19:28:00 +0100

    Create mapgen.lua

---
+++ mapgen.lua
@@ -0,0 +1,10 @@
+minetest.register_ore({
+    ore_type       = "???",
+    ore            = "chocolate:chocolate_ore",
+    wherein        = "default:stone",
+    clust_scarcity = 7 * 7 * 7,
+    clust_num_ores = 4,
+    clust_size     = 2,
+    y_min          = -10000,
+    y_max          = -31000,
+})
