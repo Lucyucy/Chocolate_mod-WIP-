cd4766390184f645364a4ae7aec518d4c0b82991
_Lucy <80708819+Lucyucy@users.noreply.github.com>
2021-03-17 18:29:27 +0100

    Update armor.lua

--- armor.lua
+++ armor.lua
@@ -1,44 +1,44 @@
 if minetest.get_modpath("3d_armor") then
-	armor:register_armor("diamond:helmet", {
-		description = ("Diamond Helmet"),
-		inventory_image = "diamond_helmet_inv.png",
-		groups = {armor_head=1, armor_heal=5, armor_use=80, armor_fire=5},
+	armor:register_armor("chocolate:helmet", {
+		description = ("Chocolate Helmet"),
+		inventory_image = "chocolate_helmet_inv.png",
+		groups = {armor_head=1, armor_heal=10, armor_use=80, armor_fire=5},
 		armor_groups = {fleshy=10},
 		damage_groups = {cracky=2, snappy=1, level=3},
 		wear = 0,
 	})
 
-	armor:register_armor("diamond:chestplate", {
-		description = ("Diamond Chestplate"),
-		inventory_image = "diamond_chestplate_inv.png",
-		groups = {armor_torso=1, armor_heal=5, armor_use=80, armor_fire=5},
+	armor:register_armor("chocolate:chestplate", {
+		description = ("Chocolate Chestplate"),
+		inventory_image = "chocolate_chestplate_inv.png",
+		groups = {armor_torso=1, armor_heal=10, armor_use=80, armor_fire=5},
 		armor_groups = {fleshy=15},
 		damage_groups = {cracky=2, snappy=1, level=3},
 		wear = 0,
 	})
 
-	armor:register_armor("diamond:leggings", {
-		description = ("Diamond Leggings"),
-		inventory_image = "diamond_leggings_inv.png",
-		groups = {armor_legs=1, armor_heal=5, armor_use=80, armor_fire=5},
+	armor:register_armor("chocolate:leggings", {
+		description = ("Chocolate Leggings"),
+		inventory_image = "chocolate_leggings_inv.png",
+		groups = {armor_legs=1, armor_heal=10, armor_use=80, armor_fire=5},
 		armor_groups = {fleshy=15},
 		damage_groups = {cracky=2, snappy=1, level=3},
 		wear = 0,
 	})
 
-	armor:register_armor("diamond:boots", {
-		description = ("Diamond Boots"),
-		inventory_image = "diamond_boots_inv.png",
-		groups = {armor_feet=1, armor_heal=5, armor_use=80, armor_fire=5, physics_jump=0.5, physics_speed = 1},
+	armor:register_armor("chocolate:boots", {
+		description = ("Chocolate Boots"),
+		inventory_image = "chocolate_boots_inv.png",
+		groups = {armor_feet=1, armor_heal=10, armor_use=80, armor_fire=5, physics_jump=3, physics_speed = 5},
 		armor_groups = {fleshy=10},
 		damage_groups = {cracky=2, snappy=1, level=3},
 		wear = 0,
 	})
 
-	armor:register_armor("diamond:shield", {
-		description = ("Diamond Shield"),
-		inventory_image = "diamond_shield_inv.png",
-		groups = {armor_shield=1, armor_heal=5, armor_use=80, armor_fire=3},
+	armor:register_armor("chocolate:shield", {
+		description = ("Chocolate Shield"),
+		inventory_image = "chocolate_shield_inv.png",
+		groups = {armor_shield=1, armor_heal=10, armor_use=80, armor_fire=3},
 		armor_groups = {fleshy=5},
 		damage_groups = {cracky=2, snappy=1, level=3},
 		wear = 0,
