6ee0141e2a9fdc58e9c1ddb20a720a57a1a72d4e
_Lucy <80708819+Lucyucy@users.noreply.github.com>
2021-03-18 21:57:04 +0100

    Update mapgen.lua

--- mapgen.lua
+++ mapgen.lua
@@ -5,6 +5,6 @@ minetest.register_ore({
     clust_scarcity = 7 * 7 * 7,
     clust_num_ores = 4,
     clust_size     = 2,
-    y_min          = -10000,
+    y_min          = -10,
     y_max          = -31000,
 })
