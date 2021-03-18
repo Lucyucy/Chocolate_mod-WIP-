8bbc209afeed9a1cc9467e107efcef9b371fe8cf
_Lucy <80708819+Lucyucy@users.noreply.github.com>
2021-03-18 09:59:31 +0100

    Update nodes.lua

--- nodes.lua
+++ nodes.lua
@@ -1,12 +1,12 @@
 minetest.register_node("chocolate:chocolate_ore", {
 		description = "Chocolate Ore",
 		tiles = {"chocolate_ore.png"},
-		groups = {cracky = 1, level = 2},
+		groups = {cracky = ?, level = ?},
 		drop = "chocolate:chocolate"
 })
 
 minetest.register_node("chocolate:chocolateblock", {
 		description = "Chocolate Block",
 		tiles = {"chocolate_block.png"},
-		groups = {cracky = 1, level = 3}
+		groups = {cracky = ?, level = ?}
 })
