ceaf305c959ae9c40e0cc04ccf0faf3b020cade2
_Lucy <80708819+Lucyucy@users.noreply.github.com>
2021-03-18 20:04:10 +0100

    Update nodes.lua

--- nodes.lua
+++ nodes.lua
@@ -1,18 +1,18 @@
 minetest.register_node("chocolate:chocolate_ore", {
 		description = "Chocolate Ore",
 		tiles = {"chocolate_ore.png"},
-		groups = {cracky = ?, level = ?},
+		groups = {cracky = 1, level = 1},
 		drop = "chocolate:chocolate"
 })
 
 minetest.register_node("chocolate:chocolate_block", {
 		description = "Chocolate Block",
 		tiles = {"chocolate_block.png"},
-		groups = {cracky = ?, level = ?}
+		groups = {cracky = 2, level = 1}
 })
 
 minetest.register_node("chocolate:power_chocolate_block", {
 		description = "Power Chocolate Block",
 		tiles = {"power_chocolate_block.png"},
-		groups = {cracky = ?, level = ?}
+		groups = {cracky = 4, level = 1}
 })
