e87700b29374212c318c4f30c8df4e3c6366a507
_Lucy <80708819+Lucyucy@users.noreply.github.com>
2021-03-20 10:51:01 +0100

    Update items.lua

--- items.lua
+++ items.lua
@@ -1,11 +1,11 @@
 minetest.register_craftitem("chocolate:chocolate", {
     description = "Chocolate",
-    inventory_image = "chocolate.png"
-        on_use = minetest.item_eat(1),
+    inventory_image = "chocolate.png",
+    on_use = minetest.item_eat(1),
 })
 
 minetest.register_craftitem("chocolate:power_chocolate", {
     description = "Power Chocolate",
-    inventory_image = "power_chocolate.png"
-        on_use = minetest.item_eat(4),
+    inventory_image = "power_chocolate.png",
+    on_use = minetest.item_eat(4),
 })
