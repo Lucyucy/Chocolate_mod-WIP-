bee1f6d6412e8bb4f4fc65c09f26812e81282fa7
_Lucy <80708819+Lucyucy@users.noreply.github.com>
2021-04-04 16:10:34 +0200

    Update mapgen.lua

--- mapgen.lua
+++ mapgen.lua
@@ -2,7 +2,7 @@ minetest.register_ore({
     ore_type       = "scatter",
     ore            = "chocolate:chocolate_ore",
     wherein        = "default:dirt_with_grass",
-    clust_scarcity = 7 * 7 * 7,
+    clust_scarcity = 1000 * 1000 * 1000,
     clust_num_ores = 4,
     clust_size     = 2,
     y_min          = 20,
