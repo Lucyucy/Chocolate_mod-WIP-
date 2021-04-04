ac2ac6341a2a14d8c0d6e9d82ba470b2cb23cc82
_Lucy <80708819+Lucyucy@users.noreply.github.com>
2021-04-04 17:20:41 +0200

    update mapgen.lua (making default:chocolate_ore more common

--- mapgen.lua
+++ mapgen.lua
@@ -1,10 +1,10 @@
 minetest.register_ore({
     ore_type       = "scatter",
     ore            = "chocolate:chocolate_ore",
-    wherein        = "default:dirt_with_grass",
-    clust_scarcity = 7 * 7 * 7,
-    clust_num_ores = 40,
-    clust_size     = 20,
-    y_min          = 40,
-    y_max          = -20,
+    wherein        = "default:stone",
+    clust_scarcity = 5 * 5 * 5,
+    clust_num_ores = 2,
+    clust_size     = 1,
+    y_min          = -31000,
+    y_max          = -10,
 })
